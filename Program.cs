70b81fc848a8e9a2866a85df422a81fcf1c87250
JPreault <99177462+JPreault@users.noreply.github.com>
2023-09-25 11:32:10 +0200

    Initialisation du gitignore

--- Program.cs
+++ Program.cs
@@ -1,17 +1,11 @@
-﻿using System.Reflection;
-//using dotenv.net;
-
-namespace microsoft.botsay;
+﻿namespace microsoft.botsay;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        //Dotenv.Load();
-
-        //string databaseUrl = Environment.GetEnvironmentVariable("CHAT_GPT_KEY");
         Console.Write("Ecris ! : ");
-        string text = Console.ReadLine();
+        string? text = Console.ReadLine();
         Console.WriteLine(text);
     }
 }
