5ac0fc2a62ccd9866415377ec457018257a92bb0
melim <lvsqmelanie@gmail.com>
2023-09-26 16:01:37 +0200

    update eletron app

--- electron-app/controller/index.js
+++ electron-app/controller/index.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get a reference to the button element by its ID
-    const createProjectButton = document.getElementById('createProjectButton');
+  // Get a reference to the button element by its ID
+  const createProjectButton = document.getElementById('createProjectButton')
 
-    // Add a click event listener to the button
-    createProjectButton.addEventListener('click', createNewProj);
+  // Add a click event listener to the button
+  createProjectButton.addEventListener('click', createNewProj)
 
-    function createNewProj() {
-        // TODO: call CLI command
-        alert("Créer un nouveau projet");
-        document.location.href="../view/newProject.html";
-    }
-});
+  function createNewProj () {
+    // TODO: call CLI command
+    document.location.href = '../view/newProject.html'
+  }
+})
--- electron-app/main.js
+++ electron-app/main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow } = require('electron')
-const { exec } = require('child_process');
+const { exec } = require('child_process')
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -11,8 +11,8 @@ const createWindow = () => {
 }
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+  if (process.platform !== 'darwin') app.quit()
+})
 
 app.whenReady().then(() => {
   createWindow()
@@ -24,18 +24,18 @@ app.whenReady().then(() => {
   })
 })
 
-const command = "mjj test Un magasins de tests de lunettes et de lasers pour enfants";
+const command = 'mjj test Un magasins de tests de lunettes et de lasers pour enfants'
 
-  exec(command, (error, stdout, stderr) => {
+exec(command, (error, stdout, stderr) => {
   if (error) {
-      console.error(`Error executing ${command}: ${error.message}`);
-      return;
+    console.error(`Error executing ${command}: ${error.message}`)
+    return
   }
 
-  console.log(`Command output:\n${stdout}`);
+  console.log(`Command output:\n${stdout}`)
   if (stderr) {
-      console.error(`Command error:\n${stderr}`);
+    console.error(`Command error:\n${stderr}`)
   }
-  });
+})
 
 console.log('Hello from Electron 👋')
